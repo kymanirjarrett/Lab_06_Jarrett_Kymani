b622a8810f000b5a5f94e80e4445618308cf836b
kymanirjarrett <krj.kymani13@gmail.com>
2024-02-12 10:25:56 -0500

    Initial commit

---
+++ src/CtoFConverter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class CtoFConverter
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String trash;
+        boolean done = false;
+
+        ...
+
+        do
+        {
+            ...
+
+        } while (!done);
+
+
+
+    }
+}
